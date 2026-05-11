7039deeaa5ddd34061671bdff9a3bf7727297780
yinshubin <603478836@qq.com>
2026-05-11 15:06:54 +0800

    origin

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+// use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    // return Inertia::render('Welcome');
+    return redirect('/admin');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+// require __DIR__ . '/settings.php';
+// require __DIR__ . '/auth.php';
